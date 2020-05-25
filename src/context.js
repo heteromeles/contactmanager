d7a10f54d13d4df8f6d3438c3d255b843bb3227c
Michael Seales <heteromeles@users.noreply.github.com>
2020-05-25 08:08:47 -0700

    After Chap5: Controlled Components & onChange

--- src/context.js
+++ src/context.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 const Context = React.createContext();
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "DELETE_CONTACT":
       return {
         ...state,
-        contacts: state.contacts.filter((contact) => contact.id != action.payload),
+        contacts: state.contacts.filter((contact) => contact.id !== action.payload),
       };
     default:
       return state;
